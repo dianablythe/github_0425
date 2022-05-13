84f7a153f863f8bfb9787a01a5c662d79dbfe81d
dianaBlythe <101481487+dianablythe@users.noreply.github.com>
2022-05-12 18:42:51 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,106 @@
-/* //testing the API
+//accessing the HTML elements
+const beerDetails = document.querySelector('.beer-details');
+const beerList = document.getElementById('beer-list');
+const beerName = document.getElementById('beer-name');
+const beerImgine = document.getElementById('beer-image');
+const reviewForm = document.getElementById('review-form');
+const beerPrice = document.getElementById('beer-price');
+const weeklyUpdate = document.getElementById('weekly-updates');
+//const beerRating = document.getElementById('beer-rating');
 
-// your code here
-const baseUrl = 'https://api.openbrewerydb.org/breweries'
-
-/* const cakeDetails = document.querySelector('.cake-details')
-
-const cakeList = document.getElementById('cake-list')
-const cakeName = document.getElementById('cake-name')
-const cakeImage = document.getElementById('cake-image')
-const cakeDescription = document.getElementById('cake-description')
-
-const descriptionForm = document.getElementById('description-form')
-const reviewForm = document.getElementById('review-form')
-const reviewList = document.getElementById('review-list')
-
-// fetch(`${baseUrl}/cakes/1`)
-//   .then(res => res.json())
-//   .then(cake => renderCake(cake))
-
-// fetch(`${baseUrl}/cakes`)
-//   .then(res => res.json())
-//   .then(cakes => {
-//     renderMenu(cakes)
-// })
-*/
-
-/* function getCakes() {
-fetch(`${baseUrl}`)
+//getting the data
+fetch('https://api.sampleapis.com/beers/stouts')
     .then(resp => resp.json())
-    .then(cakes => console.log(cakes))
+    .then(beers => {
+        renderBeers(beers);
+        renderOneBeer(beers[21]);
+        console.log(beers);
+    })
+
+    // rendering beers in the beer-list column 
+function renderBeers(beers) {
+    beerList.innerHTML = '';
+    beers.forEach(beer => {
+        const li = document.createElement('li');
+        li.innerText = beer.name;
+        //adding list elements of beers to nav bar
+        beerList.appendChild(li);
+        // click event added for each beer + img + price
+        li.addEventListener('click', () => {
+            beerName.innerText = beer.name;
+            beerImgine.src = beer.image;
+            beerPrice.innerText = beer.price;
+
+            //const li = document.getElementById('beer-rating')
+            //li.innerText = beer.reviews;
+            //beerRating.appendChild(li);
+            
+        })
+    })
+
+    const filterBtn = document.getElementById('good-beer-filter')
+
+    filterBtn.addEventListener('click', () => {
+ 
+        // filter button
+           if (filterBtn.innerText === '5 Star Filter: ON') {
+               //barDiv.innerText = ''
+       
+               beers.forEach(beer => {
+                   const span = document.createElement('span')
+                   span.innerText = beer.name
+                   span.addEventListener('click', renderBeers)
+                  //barDiv.appendChild(span)
+               })
+           //  filterBtn.innerText = '5 Star Filter: OFF'
+               filterBtn.innerText = '5 Star Filter: OFF'
+           } else if (filterBtn.innerText === '5 Star Filter: OFF') {
+           // filterBtn.innerText = '5 Star Filter: ON'
+           let result = beers.filter(x => x['rating'] > 4.5 == true)
+               //barDiv.innerText = ''
+       
+               result.forEach(beer => {
+                   const span = document.createElement('span')
+                   span.innerText = beer.name
+                   span.addEventListener('click', renderBeers)
+                   //barDiv.appendChild(span)
+               })
+               filterBtn.innerText = '5 Star Filter: ON'
+           }
+       }) 
 }
+            //trying to get the a submit event to happen for the email subcription form
+        // weeklyUpdate.addEventListener('submit', (e) =>  handleFormSubmit(e));
 
-getCakes() */
+        // function handleFormSubmit(e) {
+        //     e.preventDefault();
 
+        //     const li = document.createElement('li');   
+        //     li.innerText = weeklyUpdate.value;
 
-/* let request = new Request('https://api.openbrewerydb.org/breweries', {
-    headers: new Headers({
-        'Content-Type': 'text/json' 
-    }),
-    method: 'GET'
-}); */
+        //      reviewList.appendChild(li);
 
-/* fetch('https://api.openbrewerydb.org/breweries').then((response) => {
-    resp => resp.json()
-    console.log(response);
-    return response.text;
+        //     weeklyUpdate.reset();
+            
+       //}
+            // just rendering one beer(img) when the page loads (the nitro can)
+        function renderOneBeer(beer) {
+            beerImgine.src = beer.image;
+        }
+
+            // trying to add a review submit event to the review form, so far its cereating a list under the add review button
+         reviewForm.addEventListener('submit', (e) => handleReviewSubmit(e));
+
+         function handleReviewSubmit(e) {
+             e.preventDefault();
+
+             const li = document.createElement('li');
+             li.innerText = e.target.review.value;
+             reviewForm.appendChild(li);
+
+             reviewForm.reset();
+         }
+
     
-}); */
 
-fetch(`https://api.openbrewerydb.org/breweries`)
-    .then(resp => resp.json())
-    .then(breweries => console.log(breweries))
-
-    
+//how do I specify that I want the average of the rating to be >4.5?
